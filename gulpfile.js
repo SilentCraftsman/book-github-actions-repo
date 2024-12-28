8d0dc8821e24fd3d8dcdd176264a7b6f923ceb2f
SilentCraftsman <155365544+SilentCraftsman@users.noreply.github.com>
2024-12-28 07:53:04 +0100

    Test something with gulpfile.js 2

--- gulpfile.js
+++ gulpfile.js
@@ -2,34 +2,41 @@ var gulp = require("gulp"),
   pug = require("gulp-pug-3"),
   runSequence = require("gulp4-run-sequence");
 
+// Tâche pour compiler Pug en HTML
 gulp.task("pugToHtml", () => {
   return gulp
-    .src("src/*.pug") // S'assurer que tous les fichiers .pug dans src sont capturés
+    .src("src/*.pug") // Assurez-vous que index.pug est dans ce dossier
     .pipe(
       pug({
-        pretty: true,
+        pretty: true, // Génère un HTML bien formaté
       })
     )
-    .pipe(gulp.dest("dest")); // Le résultat doit aller dans dest/
+    .pipe(gulp.dest("dest")); // Sauvegarde les fichiers générés dans dest/
 });
 
+// Tâche pour copier les fichiers statiques (styles, images, etc.) vers dest/
 gulp.task("copy", () => {
   return gulp
     .src(
       [
-        "src/styles/*.*", // Assurer que tous les fichiers CSS sont pris en compte
+        "src/styles/*.*", // Ajouter ici les autres fichiers statiques à copier
+        "src/images/*.*", // Exemple : copier des images si nécessaire
       ],
       {
         dot: true,
       }
     )
-    .pipe(gulp.dest("dest")); // Les fichiers CSS seront également copiés dans dest/
+    .pipe(gulp.dest("dest")); // Sauvegarde dans le dossier dest/
 });
 
+// Tâche principale pour construire le projet (compile Pug + copie les fichiers statiques)
 gulp.task("build", (done) => {
   runSequence(
-    "pugToHtml", // D'abord transformer Pug en HTML
-    "copy", // Puis copier les fichiers CSS dans dest
+    ["pugToHtml"], // Compile Pug en HTML
+    ["copy"], // Copie les fichiers statiques
     done
   );
 });
+
+// Tâche par défaut (optionnelle) pour lancer le processus de build
+gulp.task("default", gulp.series("build"));
