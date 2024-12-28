1e1a33c88a9e689e517d1c5109cb02cc79df81d0
SilentCraftsman <155365544+SilentCraftsman@users.noreply.github.com>
2024-12-28 08:11:57 +0100

    test

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require("gulp"),
   pug = require("gulp-pug-3"),
-  runSequence = require("gulp4-run-sequence");
+  runSequence = require("gulp4-run-sequence"),
+  fs = require("fs"),
+  path = "src/images";
 
 // Tâche pour compiler Pug en HTML
 gulp.task("pugToHtml", () => {
@@ -16,17 +18,23 @@ gulp.task("pugToHtml", () => {
 
 // Tâche pour copier les fichiers statiques (styles, images, etc.) vers dest/
 gulp.task("copy", () => {
-  return gulp
-    .src(
-      [
-        "src/styles/*.*", // Ajouter ici les autres fichiers statiques à copier
-        "src/images/*.*", // Exemple : copier des images si nécessaire
-      ],
-      {
-        dot: true,
-      }
-    )
-    .pipe(gulp.dest("dest")); // Sauvegarde dans le dossier dest/
+  // Vérifie si le dossier src/images existe et s'il n'est pas vide
+  if (fs.existsSync(path) && fs.readdirSync(path).length > 0) {
+    return gulp
+      .src(
+        [
+          "src/styles/*.*", // Ajouter ici les autres fichiers statiques à copier
+          "src/images/*.*", // Exemple : copier des images si nécessaire
+        ],
+        {
+          dot: true,
+        }
+      )
+      .pipe(gulp.dest("dest")); // Sauvegarde dans le dossier dest/
+  } else {
+    console.log("Le dossier src/images est vide ou n'existe pas");
+    return gulp.src("src/styles/*.*", { dot: true }).pipe(gulp.dest("dest")); // Copie seulement les styles si images est vide
+  }
 });
 
 // Tâche principale pour construire le projet (compile Pug + copie les fichiers statiques)
